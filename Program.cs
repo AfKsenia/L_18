7242e25adb38a2c5e86b97c3879662cdac949280
AfKsenia <afksenia@gmail.com>
2021-11-27 20:55:26 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Stack<char> Chars = new Stack<char>();
+
+            Console.WriteLine("Введите символы");
+            string strChars = Console.ReadLine();
+
+            foreach (char item in strChars)
+            {
+                if (item == '(' || item == '{' || item == '[')
+                {
+                    Chars.Push(item);
+                }
+                if (item == ')' && Chars.Contains('('))
+                {
+                    Chars.Pop();
+                }
+                if (item == '}' && Chars.Contains('{'))
+                {
+                    Chars.Pop();
+                }
+                if (item == ']' && Chars.Contains('['))
+                {
+                    Chars.Pop();
+                }
+            }
+                if (Chars.Count==0)
+                {
+                    Console.WriteLine("Скобки расставлены корректно");
+                }
+                else
+                {
+                    Console.WriteLine("Скобки расставлены некорректно");
+                }
+                Console.ReadKey();
+            
+        }
+    }
+}
